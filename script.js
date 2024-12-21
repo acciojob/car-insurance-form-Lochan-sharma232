6732e42f89188ff78029c4062245c81d87f3c9e6
Lochan-sharma232 <lochansharma537@gmail.com>
2024-12-21 11:39:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-//your code here
-<label for="cars">Select a car </label>
-		<select placeholder="--Please choose an option--" id="cars" required>
-			<option>BMW</option>
-			<option>Audi</option>
-			<option>Tata</option>
-		</select>
+
